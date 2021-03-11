5cbb84bebc19a7f855583af701f13f5f038c5203
matheus sanches miranda <sanchesmiranda.matheus@gmail.com>
2021-03-11 17:47:44 -0300

    ajustes

--- lib/src/material/widgets/widgets.dart
+++ lib/src/material/widgets/widgets.dart
@@ -1,5 +1,8 @@
 library components;
 
+export 'package:components_venver/src/material/widgets/c_appbar.dart';
 export 'package:components_venver/src/material/widgets/c_button.dart';
+export 'package:components_venver/src/material/widgets/c_date_picker.dart';
 export 'package:components_venver/src/material/widgets/c_dropdown.dart';
 export 'package:components_venver/src/material/widgets/c_textfield.dart';
+export 'package:components_venver/src/material/widgets/c_slider.dart';
