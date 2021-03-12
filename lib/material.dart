ad88d09bfe96faaf9d41cb2d5d4b9a4f8c9bfb95
Matheus Sanches <sanchesmiranda.matheus@gmail.com>
2021-03-12 10:42:09 -0300

    ajustes

--- lib/material.dart
+++ lib/material.dart
@@ -1,5 +1,3 @@
 library components;
 
-export 'package:components_venver/src/material/icons/icons.dart';
-export 'package:components_venver/src/material/widgets/widgets.dart';
-export 'package:components_venver/src/material/transitions/transitions.dart';
+export 'package:components_venver/src/material/material.dart';
