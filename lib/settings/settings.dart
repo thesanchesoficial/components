2ed2536809515eb42f9dec5d6831116fc3744948
matheus sanches miranda <sanchesmiranda.matheus@gmail.com>
2021-03-14 21:41:10 -0300

    ajustes no components

--- lib/settings/settings.dart
+++ lib/settings/settings.dart
@@ -1,3 +1,4 @@
+library components;
 class Settings {
   
 }
--- lib/src/components.dart
+++ lib/src/components.dart
@@ -1,3 +1,4 @@
+library components;
 // ignore: non_constant_identifier_names
 import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
