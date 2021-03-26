551530282e4fd6fd061196ffa2195beec1b1490b
matheus sanches miranda <sanchesmiranda.matheus@gmail.com>
2021-03-26 14:08:26 -0300

    ajustes

--- lib/material.dart
+++ lib/material.dart
@@ -2,3 +2,4 @@ library components;
 
 export 'package:components_venver/src/material/material.dart';
 export 'package:components_venver/src/functions/print.dart';
+export 'package:components_venver/theme/app_theme.dart';
