06469b967eeaca25d5ad1d1bbf782b8fcc54ec93
matheus sanches miranda <sanchesmiranda.matheus@gmail.com>
2021-03-26 12:47:12 -0300

    ajustes

---
+++ lib/src/material/widgets/c_navigation_item.dart
@@ -0,0 +1,28 @@
+// import 'package:flutter/material.dart';
+
+// class NavigationItem extends StatelessWidget {
+//   final Widget icon;
+//   final String label;
+//   final String tooltip;
+//   final Color color;
+//   final Widget iconActive;
+//   final EdgeInsets padding;
+//   const NavigationItem({this.icon, this.label, this.color, this.iconActive, this.tooltip});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return NavigationRailDestination(
+//       icon: icon,
+//       label: Text(label),
+//       padding: padding
+//     );
+
+//     return BottomNavigationBarItem(
+//       icon: icon,
+//       label: label,
+//       tooltip: tooltip ?? label,
+//       backgroundColor: color,
+//       activeIcon: iconActive
+//     );
+//   }
+// }
