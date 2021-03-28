26336eb7f9bf1adc6b21c1f8c769217d5f290380
matheus sanches miranda <sanchesmiranda.matheus@gmail.com>
2021-03-28 13:21:04 -0300

    ajustes

--- lib/src/material/components/widgets/w_stories.dart
+++ lib/src/material/components/widgets/w_stories.dart
@@ -1,34 +1,34 @@
-import 'package:flutter/material.dart';
-import 'package:story_view/story_view.dart';
+// import 'package:flutter/material.dart';
+// import 'package:story_view/story_view.dart';
 
-class OwStoryView extends StatelessWidget {
-  final StoryController controller;
-  final List<StoryItem> storyItems;
-  final bool repeat;
-  final Function onStoryShow;
-  final Function onComplete;
+// class OwStoryView extends StatelessWidget {
+//   final StoryController controller;
+//   final List<StoryItem> storyItems;
+//   final bool repeat;
+//   final Function onStoryShow;
+//   final Function onComplete;
 
-  const OwStoryView({
-    this.controller,
-    this.storyItems,
-    this.repeat = true,
-    this.onStoryShow,
-    this.onComplete,
-  }) : super();
+//   const OwStoryView({
+//     this.controller,
+//     this.storyItems,
+//     this.repeat = true,
+//     this.onStoryShow,
+//     this.onComplete,
+//   }) : super();
 
-  @override
-  Widget build(BuildContext context) {
-    return StoryView(
-      storyItems: storyItems,
-      controller: controller,
-      repeat: repeat,
-      onStoryShow: onStoryShow ?? (s) {print(s.toString());},
-      onComplete: onComplete ?? () {},
-      onVerticalSwipeComplete: (direction) {
-        if (direction == Direction.down) {
-          Navigator.pop(context);
-        }
-      }
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return StoryView(
+//       storyItems: storyItems,
+//       controller: controller,
+//       repeat: repeat,
+//       onStoryShow: onStoryShow ?? (s) {print(s.toString());},
+//       onComplete: onComplete ?? () {},
+//       onVerticalSwipeComplete: (direction) {
+//         if (direction == Direction.down) {
+//           Navigator.pop(context);
+//         }
+//       }
+//     );
+//   }
+// }
